c95ed08abf49a6c31648daf9ac5efeb573405943
vidhyaal <vidhya_al@yahoo.com>
2019-11-21 21:46:26 +0800

    Success page url change

--- Credit.php
+++ Credit.php
@@ -10,7 +10,7 @@ session_start();
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <body>
-<form name="ccForm" method="post" action='/cart/success.php'>
+<form name="ccForm" method="post" action='success.php'>
 <div class="container">
 <div class="row">
 <!-- You can make it whatever width you want. I'm making it full width
