97fdef85cc629665641e9f3e70f2fc36b8e27627
vidhyaal <vidhya_al@yahoo.com>
2019-11-21 21:44:15 +0800

    payment status check

--- success.php
+++ success.php
@@ -1,6 +1,12 @@
 <?php
 session_start();
 $res=$_SESSION['result'];
+if $result['resultCode']=='Authorised{
+echo "<body><h1>Payment Success!</h1></body>";
+}
+else
+echo "<body><h1>Payment Refused!</h1></body>";
+
 echo "<pre>";
 print_r($res);
 ?>
@@ -11,6 +17,6 @@ print_r($res);
         </title>
     </head>
    
-    <body><h1>Payment Success!</h1></body>
+    
     
 </html>
