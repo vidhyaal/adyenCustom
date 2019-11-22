b09253676f74b860f695f90d8b6bd421cc7edd84
vidhyaal <vidhya_al@yahoo.com>
2019-11-22 22:44:58 +0800

    Update final.php

--- final.php
+++ final.php
@@ -1,10 +1,8 @@
 <?php
-session_start();
-
 require __DIR__ . '/vendor/autoload.php';
 // Set your X-API-KEY with the API key from the Customer Area.
 $client = new \Adyen\Client();
-$client->setXApiKey("AQE1hmfxJo/OaR1Kw0m/n3Q5qf3Ve55dHZxYTFdTxWq+l3JOk8J4BA+ztqPuS4G6vYkJoj+c4MIQwV1bDb7kfNy1WIxIIkxgBw==-+g+FdHJB1s/8mcg/evWIDkJgGZRqO7hMsodFya1e7f0=-Bg2vV8qEgP65bdMd");
+$client->setXApiKey("AQE1hmfxJo/OaR1Kw0m/n3Q5qf3Ve55dHZxYTFdTxWq+l3JOk8J4BA+ztqPuS4G6vYkJoj+c4MIQwV1bDb7kfNy1WIxIIkxgBw==-cpyGy0LKZtWHM3D+Mk38kzhN3F+nlAtza4Rf9jFsNO8=-h4tyr9I25QRa4GEV");
 $client->setEnvironment(\Adyen\Environment::TEST);
 $service = new \Adyen\Service\Checkout($client);
  
@@ -16,20 +14,17 @@ $params = array(
   "reference" => "12345",
   "paymentMethod" => array(
     "type" => "scheme",
-    "number" =>"4111111111111111",
+    "number" =>"4111111145551142",
     "expiryMonth"=> "10",
     "expiryYear"=> "2020",
     "holderName"=>"John Smith",
     "cvc"=> "737"
   ),
-  "returnUrl" => "success.php",
+  "returnUrl" => "final.php",
   "merchantAccount" => "SupportRecruitementCOM"
 );
 $result = $service->payments($params);
 $_SESSION['result'] = $result;
- $_COOKIE['res'] = $result;
- echo "<pre>";
- print_r($result) ;
-
-
+echo '<pre>';
+print_r($result) ;
 ?>
