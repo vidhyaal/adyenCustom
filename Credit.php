9e6f6a37d4ce5e6a5d72680f6596f917072f2d71
vidhyaal <vidhya_al@yahoo.com>
2019-11-24 01:56:21 +0800

    Beautify CC form

--- Credit.php
+++ Credit.php
@@ -4,96 +4,68 @@ session_start();
 
 <html>
 <head><title>CC Form</title>
-<link rel="stylesheet" href="credit.css">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css" />
+
+    <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
 <body>
 <form name="ccForm" method="post" action='final.php'>
+
 <div class="container">
-<div class="row">
-<!-- You can make it whatever width you want. I'm making it full width
-on <= small devices and 4/12 page width on >= medium devices -->
-<div class="col">
-
-
-<!-- CREDIT CARD FORM STARTS HERE -->
-<div class="panel panel-default credit-card-box">
-<div class="panel-heading display-table" >
-<div class="row display-tr" >
-<h3 class="panel-title display-td" >Payment Details</h3>
-<div class="display-td" >                            
-<img class="img-responsive pull-right" src="http://i76.imgup.net/accepted_c22e0.png">
-</div>
-</div>                    
-</div>
-<div class="panel-body">
-<form role="form" id="payment-form">
-<div class="row">
-<div class="col">
-<div class="form-group">
-<label for="cardNumber">CARD NUMBER</label>
-<div class="input-group">
-<input 
-type="tel"
-class="form-control"
-name="cardNumber"
-placeholder="Valid Card Number"
-autocomplete="cc-number"
-required autofocus 
-/>
-<span class="input-group-addon"><i class="fa fa-credit-card"></i></span>
-</div>
-</div>                            
-</div>
-</div>
-<div class="row">
-<div class="col-xs-7 col-md-7">
-<div class="form-group">
-<label for="cardExpiry"><span class="hidden-xs">EXPIRATION</span><span class="visible-xs-inline">EXP</span> DATE</label>
-<input 
-type="tel" 
-class="form-control" 
-name="cardExpiry"
-placeholder="MM / YY"
-autocomplete="cc-exp"
-required 
-/>
-</div>
-</div>
-<div class="col-xs-5 col-md-5 pull-right">
-<div class="form-group">
-<label for="cardCVC">CV CODE</label>
-<input 
-type="tel" 
-class="form-control"
-name="cardCVC"
-placeholder="CVC"
-autocomplete="cc-csc"
-required
-/>
-</div>
-</div>
+    <div class="row">
+        <div class="col-xs-12 col-md-4 col-md-offset-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <div class="row">
+                        <h3 class="text-center">Payment Details</h3>
+                        <!--img class="img-responsive cc-img" src="http://www.prepbootstrap.com/Content/images/shared/misc/creditcardicons.png"-->
+                    </div>
+                </div>
+                <div class="panel-body">
+                    <form role="form">
+                        <div class="row">
+                            <div class="col-xs-12">
+                                <div class="form-group">
+                                    <label>CARD NUMBER</label>
+                                    <div class="input-group">
+                                        <input type="tel" class="form-control" placeholder="Valid Card Number" />
+                                        <span class="input-group-addon"><span class="fa fa-credit-card"></span></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-7 col-md-7">
+                                <div class="form-group">
+                                    <label><span class="hidden-xs">EXPIRATION</span><span class="visible-xs-inline">EXP</span> DATE</label>
+                                    <input type="tel" class="form-control" placeholder="MM / YY" />
+                                </div>
+                            </div>
+                            <div class="col-xs-5 col-md-5 pull-right">
+                                <div class="form-group">
+                                    <label>CV CODE</label>
+                                    <input type="tel" class="form-control" placeholder="CVC" />
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </form>
+                </div>
+                <div class="panel-footer">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <button class="btn btn-success btn-lg btn-block" >Process payment</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
-<div class="row">
-<div class="col">
-<button class="btn btn-success btn-lg btn-block" type="submit" onClick="alert('Form submitted!')">Pay Now</button>
-</div>
-</div>
-<div class="row" style="display:none;">
-<div class="col-xs-12">
-<p class="payment-errors"></p>
-</div>
-</div>
-</form>
-</div>
-</div>            
+
 <!-- CREDIT CARD FORM ENDS HERE -->
-</div>            
-</div>
-</div>
 </form>
 </body>
 </html>
